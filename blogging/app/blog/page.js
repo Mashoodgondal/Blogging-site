5a44d0041e9dca367c6544c9a6c83c0ac610b220
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-06-01 22:34:02 +0500

    page routing in site

---
+++ blogging/app/blog/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div>Blog page in site</div>
+    )
+}
+
+export default Blog
---
+++ blogging/app/home/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HOME = () => {
+    return (
+        <div>HOME page in site</div>
+    )
+}
+
+export default HOME
