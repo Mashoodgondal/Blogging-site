713efcc7b0b295353f35298f87d7d284a7760343
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-06-01 22:35:17 +0500

    page routing in site

--- blogging/app/blog/page.js
+++ blogging/app/blog/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Blog = () => {
     return (
-        <div>Blog page in site</div>
+        <div>Blog page in site
+            <h2>Here are all blogs are showing</h2>
+        </div>
     )
 }
 
--- blogging/app/home/page.js
+++ blogging/app/home/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HOME = () => {
     return (
-        <div>HOME page in site</div>
+        <div>HOME page in site
+            <h2>Here is the home page of the site</h2>
+        </div>
     )
 }
 
