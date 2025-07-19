b996e738e757e4aca1765ca989e1ebbfa3dc9466
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-07-19 22:01:29 +0500

    add home page

--- blogging/app/home/page.js
+++ blogging/app/home/page.js
@@ -25,3 +25,18 @@ export default HOME
 // }
 
 // export default HOME
+
+
+
+// import React from 'react'
+
+// const HOME = () => {
+//     return (
+//         <div>HOME page in site
+
+
+//         </div>
+//     )
+// }
+
+// export default HOME
